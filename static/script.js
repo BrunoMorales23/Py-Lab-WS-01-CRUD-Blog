1597c6a81b16dd8d8dd146bc85a85232e7deb7ef
Bruno Morales <brunomorales0323@outlook.es>
2025-05-14 17:50:20 -0300

    Styles in progress

--- static/script.js
+++ static/script.js
@@ -1,8 +1,10 @@
 function createPost() {
     document.getElementById("createPostContainer").style.display = "block";
+    document.getElementById("contentList").style.display = "none";
     return true;
 }
 
 function closePost() {
     document.getElementById("createPostContainer").style.display = "none";
+    document.getElementById("contentList").style.display = "block";
 }
