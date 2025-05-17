57305a856b1531b642532075a9ac83ec0f475ad5
Bruno Morales <brunomorales0323@outlook.es>
2025-05-17 16:23:12 -0300

    Boostrap Styles added to 'refLog'

--- static/script.js
+++ static/script.js
@@ -1,10 +1,12 @@
 function createPost() {
     document.getElementById("createPostContainer").style.display = "block";
     document.getElementById("contentList").style.display = "none";
+    document.getElementById("logContent").style.display = "none";
     return true;
 }
 
 function closePost() {
     document.getElementById("createPostContainer").style.display = "none";
     document.getElementById("contentList").style.display = "block";
+    document.getElementById("logContent").style.display = "block";
 }
